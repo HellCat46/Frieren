88817adab976bbd79c58dbbff74f9f81a5f61003
HellCat46 <hellcat2388@gmail.com>
2023-11-07 14:19:23 +0530

    Option to select a topic from a list

--- README.md
+++ README.md
@@ -4,8 +4,9 @@
 - [ ] Option to Delete topic and anything associated with it
 - [ ] Option to Mark a topic as archived
 - [ ] Get a pdf made of topic's images when marked as archived
-- [ ] Option to select a topic from a list (maybe combine it with getTopic?)
+- [X] Option to select a topic from a list (maybe combine it with getTopic?)
 
 # Bugs and other Issues
 - [ ] Use DB drivers because ORMs aren't for me
 - [ ] Make other options unaccessable when one of options is being processed
+- [ ] After selecting a topic from Selection Menu, the Back button is automatically clicked.
--- bot/src/commands/getTopic.ts
+++ bot/src/commands/getTopic.ts
@@ -1,4 +1,14 @@
-import { AutocompleteInteraction, ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+  ActionRowBuilder,
+  AutocompleteInteraction,
+  ChatInputCommandInteraction,
+  ComponentType,
+  EmbedBuilder,
+  SlashCommandBuilder,
+  StringSelectMenuBuilder,
+  StringSelectMenuInteraction,
+  StringSelectMenuOptionBuilder,
+} from "discord.js";
 import { embedError, embedTopic } from "../components/EmbedTemplate";
 import { getPageLink } from "../components/Requests";
 
@@ -6,36 +16,92 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("gettopic")
     .setDescription("Get Embed of existing Topic")
-    .addIntegerOption( (options) => options
-        .setName("id")
-        .setDescription("Name of the Topic")
-        .setRequired(true)
+    .addIntegerOption((options) =>
+      options.setName("id").setDescription("ID of the Topic")
     ),
-    async execute(interaction : ChatInputCommandInteraction){
-      await interaction.deferReply()
-      const id = interaction.options.getInteger("id", true);
-      const topic = interaction.client.Topics.get(id);
+  async execute(interaction: ChatInputCommandInteraction) {
+    await interaction.deferReply();
+    let id = interaction.options.getInteger("id");
 
-      if(!topic){
-        await interaction.editReply({embeds : [embedError("No Topic Exists with this id")]})
-        return;
-      }
-      if(topic.page_count == 0){
-        const message = embedTopic(id, topic.name);
-        await interaction.editReply({
-          embeds: [message.embed],
-          components: message.rows.slice(1),
-        });
-        return;
-      }
-          
-      const link = await getPageLink(interaction.client.api_url, id, 1);
-      if (!link.startsWith("http")) {
-            await interaction.editReply({ embeds: [embedError(link)] });
-            return;
-      }
-      
-      const message = embedTopic(id, topic.name, `1 of ${topic.page_count}`, link)
-      await interaction.editReply({embeds : [message.embed], components : message.rows})
+    if (id != null) {
+      await Response(interaction, id);
+      return;
     }
+
+    let selection = new StringSelectMenuBuilder()
+      .setCustomId("topicSelection")
+      .setPlaceholder("Select one of these topics");
+
+    for (const topic of interaction.client.Topics) {
+      selection.options.push(
+        new StringSelectMenuOptionBuilder()
+          .setValue(`${topic[0]}`)
+          .setLabel(`${topic[1].name}`)
+          .setDescription(`This topic has ${topic[1].page_count} pages.`)
+      );
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle("Topic Selector")
+      .setDescription(
+        "Don't remember the ID of the topic? Worry not. Select a topic from the selection menu below."
+      )
+      .setFooter({ text: "The Menu will disappear after 2 minutes" });
+
+    (
+      await interaction.editReply({
+        embeds: [embed],
+        components: [
+          new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+            selection
+          ),
+        ],
+      })
+    )
+      .awaitMessageComponent({
+        filter: (selection) => selection.user.id == interaction.user.id,
+        time: 120000,
+        componentType: ComponentType.StringSelect,
+      })
+      .then(async (value) => await Response(interaction, +value.values[0]))
+      .catch(
+        async () =>
+          await interaction.editReply({
+            content: "Timer Ended",
+            embeds: [],
+            components: [],
+          })
+      )
+  },
 };
+
+
+async function Response(interaction: ChatInputCommandInteraction, id: number) {
+  const topic = interaction.client.Topics.get(id);
+  if (!topic) {
+    await interaction.editReply({
+      embeds: [embedError("No Topic Exists with this id")],
+    });
+    return;
+  }
+  if (topic.page_count == 0) {
+    const message = embedTopic(id, topic.name);
+    await interaction.editReply({
+      embeds: [message.embed],
+      components: message.rows.slice(1),
+    });
+    return;
+  }
+
+  const link = await getPageLink(interaction.client.api_url, id, 1);
+  if (!link.startsWith("http")) {
+    await interaction.editReply({ embeds: [embedError(link)] });
+    return;
+  }
+
+  const message = embedTopic(id, topic.name, `1 of ${topic.page_count}`, link);
+  await interaction.editReply({
+    embeds: [message.embed],
+    components: message.rows,
+  });
+}
