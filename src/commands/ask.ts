1fe4b3c4e63bb24cdff6a92d577aed240b40d43d
HellCat46 <hellcat2388@gmail.com>
2024-02-16 19:04:27 +0530

    Add Option to Use Image with Text Prompt

--- src/commands/ask.ts
+++ src/commands/ask.ts
@@ -1,9 +1,12 @@
+import { Part } from "@google/generative-ai";
 import {
   SlashCommandBuilder,
   SlashCommandStringOption,
   ChatInputCommandInteraction,
   EmbedBuilder,
+  SlashCommandAttachmentOption,
 } from "discord.js";
+import { readFileSync } from "fs";
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,27 +17,53 @@ module.exports = {
         .setName("prompt")
         .setDescription("Thing you want to ask")
         .setRequired(true)
+    )
+    .addAttachmentOption((option: SlashCommandAttachmentOption) =>
+      option
+        .setName("imgprompt")
+        .setDescription("Prompt the AI using an Image.")
+        .setRequired(false)
     ),
   async execute(interaction: ChatInputCommandInteraction) {
     await interaction.deferReply();
     const prompt = interaction.options.getString("prompt", true);
+    const imgPrompt = interaction.options.getAttachment("imgprompt", false);   
 
-    const model = interaction.client.genAI.getGenerativeModel({model : "gemini-1.0-pro"});
-    const result = await model.generateContentStream(prompt);
+    let imgPart : string | Part;
+    if(imgPrompt != null && imgPrompt.contentType != null) {
+      const res = await fetch(imgPrompt.url);
+      imgPart = {inlineData : {data : Buffer.from(await res.arrayBuffer()).toString("base64"), mimeType : imgPrompt.contentType}};
+    }else{
+      imgPart = "";
+    }
+
+
+    const model = interaction.client.genAI.getGenerativeModel({
+      model: imgPrompt !== null ? "gemini-pro-vision" : "gemini-1.0-pro",
+    });
+    const result = await model.generateContentStream([prompt, imgPart]);
+
+
     let res = "";
     let linecount = 1;
-    
-    for await (const chunk of result.stream){
+    for await (const chunk of result.stream) {
       const chunktxt = chunk.text();
       res += chunktxt;
 
-      if(chunktxt.includes('\n')){
+      if (chunktxt.includes("\n")) {
         await interaction.editReply(`Chunk ${linecount++} Received...`);
       }
     }
 
-    const embed = new EmbedBuilder().setTitle(prompt).setDescription(res).setColor("Blue");
 
-    await interaction.editReply({content: "", embeds : [embed]});
+    let embed = new EmbedBuilder()
+      .setDescription(res)
+      .setColor("Blue");
+    if(imgPrompt !== null) embed.setImage(imgPrompt.url);
+
+    await interaction.editReply({ content: "", embeds: [embed] });
+
+    
   },
 };
+
