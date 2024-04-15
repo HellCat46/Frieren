1726b4b01bfe49b7f1d8da0919e2dc3fde9d853d
HellCat46 <hellcat2388@gmail.com>
2024-04-15 10:23:57 +0530

    Stop the Player when Bot disconnects

--- src/events/VoiceStateUpdate.ts
+++ src/events/VoiceStateUpdate.ts
@@ -1,50 +1,65 @@
 import {
   Events,
   VoiceState,
-  Client,
   EmbedBuilder,
+  Channel,
 } from "discord.js";
 import { Frieren } from "../Frieren";
+import { stopMusic } from "../components/musicPlayer";
 
 module.exports = {
   name: Events.VoiceStateUpdate,
   once: false,
   async execute(client: Frieren, oldState: VoiceState, newState: VoiceState) {
     try {
-      if (client.user?.id === newState.id && client.music.queue.length !== 0) {
-        const music = client.music.queue[0];
-        const channel = await client.channels.fetch(music.channel);
+      if (client.user?.id !== newState.id && client.music.queue.length === 0)
+        return;
+      const music = client.music.queue[0];
+      const channel = await client.channels.fetch(music.channel);
 
-        if (!oldState.mute && newState.mute) {
-          if (client.music.player.pause(true)) {
-            if (channel == null || !channel.isTextBased()) return;
+      // Stop and Clear Music Queue after Bot is disconnected from VC
+      if (oldState.channel != null && newState.channel == null) {
+        stopMusic(client, oldState.channel.guildId);
+        sendMessage(
+          channel,
+          new EmbedBuilder()
+            .setTitle("Bot was disconnected from the VC Channel")
+            .setDescription(
+              "Player has been stopped and Queue is also cleared."
+            )
+            .setColor("Red")
+            .setTimestamp()
+        );
+        return;
+      }
 
-            await channel.send({
-              embeds: [
-                new EmbedBuilder()
-                  .setTitle("Successfully Paused the Music Player")
-                  .setDescription(
-                    `The Song [${music.title}](${music.url}) has been successfully paused.`
-                  )
-                  .setColor("Yellow"),
-              ],
-            });
-          }
-        } else if (oldState.mute && !newState.mute) {
-          if (client.music.player.unpause()) {
-            if (channel == null || !channel.isTextBased()) return;
-            await channel.send({
-              embeds: [
-                new EmbedBuilder()
-                  .setTitle("Successfully Resumed the Music Player")
-                  .setDescription(
-                    `The Song [${music.title}](${music.url}) has been successfully resumed.`
-                  )
-                  .setColor("Green"),
-              ],
-            });
-          }
-        }
+      // Pauses and Resume player whenever bot is serverMute has changed.
+      if (!oldState.mute && newState.mute && client.music.player.pause(true)) {
+        sendMessage(
+          channel,
+          new EmbedBuilder()
+            .setTitle("Successfully Paused the Music Player")
+            .setDescription(
+              `The Song [${music.title}](${music.url}) has been successfully paused.`
+            )
+            .setColor("Yellow")
+        );
+        return;
+      } else if (
+        oldState.mute &&
+        !newState.mute &&
+        client.music.player.unpause()
+      ) {
+        sendMessage(
+          channel,
+          new EmbedBuilder()
+            .setTitle("Successfully Resumed the Music Player")
+            .setDescription(
+              `The Song [${music.title}](${music.url}) has been successfully resumed.`
+            )
+            .setColor("Green")
+        );
+        return;
       }
     } catch (err) {
       console.error(err);
@@ -52,9 +67,8 @@ module.exports = {
   },
 };
 
-// if(client.user?.id === userId){
-//     if(client.music.queue.length > 0){
+async function sendMessage(channel: Channel | null, embed: EmbedBuilder) {
+  if (channel == null || !channel.isTextBased()) return;
 
-//     if(client.music.player.pause(true))
-//     }
-// }
+  await channel.send({ embeds: [embed] });
+}
